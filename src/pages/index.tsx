f277790463ad8bebea7e98ddde67462886289f73
Mutsu Bojiiwa <oxidation777@gmail.com>
2024-04-29 13:35:50 +0900

    login.tsx作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { Login } from './login'
 
 export default function Home() {
   return (
@@ -6,7 +7,7 @@ export default function Home() {
       <Head>
         <title>Polyglot</title>
       </Head>
-      <div>マージの練習</div>
+      <Login />
     </>
   )
 }
