7888d5bfbcde9da4bf2dac2248a08937f5f1a5cc
Mutsu Bojiiwa <oxidation777@gmail.com>
2024-05-04 16:51:50 +0900

    ログイン画面の色合い変更

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -9,23 +9,23 @@ const Login = () => {
 
   return (
 <>
-  <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-    <div className="bg-red-200 p-8 rounded-lg shadow-md">
-      <p className="text-xl font-semibold mb-4">ログイン</p>
-      <form onSubmit={handleSubmit(handleFormSubmit)} className="flex flex-col space-y-4">
-        <label className="flex flex-col">
+  <div className="flex h-screen flex-col items-center justify-center bg-base">
+    <div className="w-96 rounded-lg bg-white p-10 shadow-md">
+      <p className="mb-10 text-center text-xl ">ログイン</p>
+      <form onSubmit={handleSubmit(handleFormSubmit)} className="flex flex-col">
+        <label className="mb-4 flex flex-col">
           <span className="mb-1">メールアドレス</span>
-          <input type="text" {...register("email")} className="border border-gray-400 p-2 rounded-md" />
+          <input type="text" {...register("email")} className="rounded-md border border-gray-400 p-2 focus:border-red-500" />
         </label>
-        <label className="flex flex-col">
+        <label className="mb-12 flex flex-col">
           <span className="mb-1">パスワード</span>
-          <input type="password" {...register("password")} className="border border-gray-400 p-2 rounded-md" />
+          <input type="password" {...register("password")} className="rounded-md border border-gray-400 p-2" />
         </label>
-        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md">ログイン</button>
+        <button type="submit" className="rounded-md bg-primary px-4 py-2 text-white">ログイン</button>
       </form>
-      <div className="flex flex-col space-y-2 mt-4">
-        <button className="bg-gray-200 text-gray-800 px-4 py-2 rounded-md">ゲストログイン</button>
-        <a className="">新規登録</a>
+      <div className="mt-12 flex flex-col">
+        <button className="rounded-md bg-secondary px-4 py-2 text-gray-800">ゲストログイン</button>
+        <a className="mt-6 text-center">新規登録</a>
       </div>
     </div>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-noto)', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          light: '#ACBFE6',
+          DEFAULT: '#6688CC',
+          dark: '#3B5D8F',
+        },
+        base: '#E5EBF4',
+        secondary: '#F2B825',
+        error: '#EA4E4E',
+      },
+    },
   },
   plugins: [],
 }
