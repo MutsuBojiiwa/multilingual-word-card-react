f87f36fd068c4d156768cfc4152feb9771a240ea
Mutsu Bojiiwa <oxidation777@gmail.com>
2024-05-18 17:17:30 +0900

    login.tsxのリファクタリング

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,41 +1,66 @@
 import axios from "axios"
 
 import { useForm } from "react-hook-form"
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'
+import z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { toast } from 'react-toastify'
+
+interface FormValues {
+  email: string
+  password: string
+}
+
+const FormSchema = z.object({
+  email: z.string().email('Invalid email address'),
+  password: z.string(),
+})
 
 const Login = () => {
-  const { register, handleSubmit } = useForm()
-  const router = useRouter();
+  const router = useRouter()
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  let token = ""
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty },
+  } = useForm<FormValues>({
+    resolver: zodResolver(FormSchema),
+  })
 
   const http = axios.create({
     baseURL: 'http://api.laravel-v10-starter.localhost/api/',
   });
 
-  const handleFormSubmit = () => {
-    const email = 'user2@example.com'
-    const password = 'password'
-      http.post('login', { email, password }).then((res) => {
-        console.log("ログインOK");
-        console.log(res);
-        token = res.data.authorization.token
-        localStorage.setItem('token', token);
-        router.push('/dashboard');
+  const onSubmit = (values: FormValues) => {
+    http.post('login', {
+      email: values.email,
+      password: values.password,
+    })
+      .then((res) => {
+        localStorage.setItem('token', res.data.authorization.token)
+        router.push('/dashboard')
+        toast.success('ログインしました')
+      })
+      .catch((e) => {
+        console.error(e)
+        toast.error('ログインに失敗しました', e)
       })
   }
 
   const handleGuestLoginClicked = () => {
-    http.get('users', {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    }).then((res) => {
-      console.log(res.data);
-    }).catch((error) => {
-      console.error(error);
-    });
+    http.post('login', {
+      email: "user@example.com",
+      password: "password",
+    })
+      .then((res) => {
+        localStorage.setItem('token', res.data.authorization.token)
+        router.push('/dashboard')
+        toast.success('ゲストユーザーでログインしました')
+      })
+      .catch((e) => {
+        console.error(e)
+        toast.error('ゲストユーザーのログインに失敗しました', e)
+      })
   }
 
   const handleSignInClicked = () => {
@@ -50,20 +75,45 @@ const Login = () => {
       <div className="flex h-screen flex-col items-center justify-center bg-base">
         <div className="w-96 rounded-lg bg-white p-10 shadow-md">
           <p className="mb-10 text-center text-xl ">ログイン</p>
-          <form onSubmit={handleSubmit(handleFormSubmit)} className="flex flex-col">
+          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
             <label className="mb-4 flex flex-col">
               <span className="mb-1">メールアドレス</span>
-              <input type="text" {...register("email")} className="rounded-md border border-gray-400 p-2 focus:border-red-500" />
+              <input
+                {...register('email', { required: 'Email is required' })}
+                type="email"
+                id="email"
+                className="rounded-md border border-gray-400 p-2 focus:border-red-500"
+                placeholder="example@test.com"
+                required
+              />
+              {errors.email && <div className="mt-2 text-sm text-error">{errors.email.message}</div>}
             </label>
             <label className="mb-12 flex flex-col">
               <span className="mb-1">パスワード</span>
-              <input type="password" {...register("password")} className="rounded-md border border-gray-400 p-2" />
+              <input
+                {...register('password', { required: 'Password is required' })}
+                type="password"
+                id="password"
+                className="rounded-md border border-gray-400 p-2"
+                placeholder="パスワードを入力..."
+                required
+              />
+              {errors.email && <div className="mt-2 text-sm text-error">{errors.email.message}</div>}
             </label>
-            <button type="submit" className="rounded-md bg-primary px-4 py-2 text-white">ログイン</button>
+            <button
+              type="submit"
+              className="rounded-md bg-primary px-4 py-2 text-white"
+              disabled={!isDirty}
+            >
+              ログイン
+            </button>
           </form>
           <div className="mt-12 flex flex-col">
-            <button onClick={handleGuestLoginClicked}
-              className="rounded-md bg-secondary px-4 py-2 text-gray-800">ゲストログイン</button>
+            <button
+              onClick={handleGuestLoginClicked}
+              className="rounded-md bg-secondary px-4 py-2 text-gray-800">
+              ゲストログイン
+            </button>
             <button onClick={handleSignInClicked} className="mx-auto mt-6 w-20 text-center">新規登録</button>
           </div>
         </div>
