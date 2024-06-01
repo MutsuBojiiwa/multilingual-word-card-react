c7289f74b437574d345a807d23df9ea04f62f187
Mutsu Bojiiwa <oxidation777@gmail.com>
2024-06-01 18:41:46 +0900

    Axiosのインターセプターを実装。毎回APIを呼ぶ時にトークンを内蔵してくれる

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,12 @@
-import { API_URL } from '@/env';
-import axios from 'axios'
+import { Api } from '@/api/ApiWrapper'
 import Head from 'next/head'
 import Link from 'next/link'
 
 export default function Home() {
 
   const handleClick = () => {
-    let http;
 
-    console.log(`API_URL = ${API_URL}`)
-
-    if (API_URL) {
-      console.log("うえ")
-
-      http = axios.create({
-        baseURL: API_URL,
-      });
-    } else {
-      console.log("した")
-
-      http = axios.create({
-        baseURL: 'http://api.laravel-v10-starter.localhost/api',
-      });
-    }
-
-    http.get('/health')
+    Api.get('/health')
     .then((res) => {
       console.log(res)
     })
