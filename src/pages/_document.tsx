392ee0d4dfa7b7e0d31032a79115e19dbef86692
Mutsu Bojiiwa <oxidation777@gmail.com>
2024-06-13 19:53:19 +0900

    tailwind cssのカスタムフォントのエラー修正

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="font-body bg-base">
+      <body className="bg-base font-sans">
         <Main />
         <NextScript />
       </body>
