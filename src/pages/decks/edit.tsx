d33e72d2a7a8b4f403a87577fe1e019fb7c4e6ac
Mutsu Bojiiwa <oxidation777@gmail.com>
2024-06-19 21:48:38 +0900

    useFieldArrayを用いて動的にテキストエリアを生成

--- src/pages/decks/edit.tsx
+++ src/pages/decks/edit.tsx
@@ -1,122 +1,120 @@
 /* eslint-disable max-lines */
-import { Api } from "@/api/ApiWrapper"
-import CustomHead from "@/components/customHead"
-import Header from "@/components/header"
-import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
-import { useForm } from "react-hook-form"
-import z from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
+import { Api } from "@/api/ApiWrapper";
+import CustomHead from "@/components/customHead";
+import Header from "@/components/header";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useForm, useFieldArray } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 type Deck = {
-  id: number,
-  userId: number,
-  name: string,
-  isFavorite: boolean,
-  isPublic: boolean
-}
+  id: number;
+  userId: number;
+  name: string;
+  isFavorite: boolean;
+  isPublic: boolean;
+};
 
 const initialDeck: Deck = {
   id: 0,
   name: "",
   userId: 0,
   isFavorite: false,
-  isPublic: false
-}
+  isPublic: false,
+};
 
 interface FormValues {
-  word: string
+  words: { word: string }[];
 }
 
 const FormSchema = z.object({
-  word: z
-    .string({
-      required_error: '単語を入力してください'
-    })
-    .max(255, '255字以内で入力してください'),
-})
+  words: z
+    .array(
+      z.object({
+        word: z.string().max(255, "255字以内で入力してください"),
+      })
+    )
+    .min(1, "少なくとも1つの単語を入力してください"),
+});
 
 const DeckEditPage = () => {
-  const [deck, setDeck] = useState(initialDeck)
-  const [cards, setCards] = useState([])
-  const [locales, setLocales] = useState([])
-  const router = useRouter();
+  const [deck, setDeck] = useState(initialDeck); 
+  const [cards, setCards] = useState([]); 
+  const [locales, setLocales] = useState([]); 
+  const router = useRouter(); 
 
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isDirty },
+    reset,
+  } = useForm<FormValues>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      words: [],
+    },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "words",
+  });
 
   useEffect(() => {
-    const { id, name, userId, isFavorite, isPublic, } = router.query
+    const { id, name, userId, isFavorite, isPublic } = router.query;
     const deck: Deck = {
       id: Number(id),
       userId: Number(userId),
       name: String(name),
       isFavorite: isFavorite === "true",
       isPublic: isPublic === "true",
-    }
-    setDeck(deck)
+    };
+    setDeck(deck);
 
-    Api.get(`/cards/${deck.id}`)
-      .then((res) => {
-        console.log(res.data)
-        setCards(res.data.cards)
-        setLocales(res.data.locales)
-      })
-  }, [router.query])
+    Api.get(`/cards/${deck.id}`).then((res) => {
+      console.log(res.data);
+      setCards(res.data.cards);
+      setLocales(res.data.locales);
+
+      reset({
+        words: res.data.locales.map(() => ({ word: "" })),
+      });
+    });
+  }, [router.query, reset]);
 
   const handleFavoriteChange = () => {
     const newDeck: Deck = {
       ...deck,
-      isFavorite: !deck.isFavorite
-    }
+      isFavorite: !deck.isFavorite,
+    };
     setDeck(newDeck);
-    console.log("newDeck")
-    console.log(newDeck)
+    console.log("newDeck");
+    console.log(newDeck);
 
     Api.put(`/decks/update/${newDeck.id}`, newDeck)
       .then((res) => {
-        console.log("put OK")
-        console.log(res)
+        console.log("put OK");
+        console.log(res);
       })
       .catch((e) => {
-        console.log(e)
-      })
-
+        console.log(e);
+      });
   };
 
   const handlePublicChange = () => {
     const newDeck: Deck = {
       ...deck,
-      isPublic: !deck.isPublic
-    }
+      isPublic: !deck.isPublic,
+    };
     setDeck(newDeck);
   };
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isDirty },
-  } = useForm<FormValues>({
-    resolver: zodResolver(FormSchema),
-  })
-
   const onSubmit = (values: FormValues) => {
-    console.log(values)
-    // Api.post('/auth/register', {
-    //   name: values.name,
-    //   email: values.email,
-    //   password: values.password,
-    // })
-    //   .then(() => {
-    //     handleLogin(values, router)
-    //   })
-    //   .catch((e) => {
-    //     if (e.response.status === 422) {
-    //       alert(e.response.data.message)
-    //     }else{
-    //       alert("ユーザーの登録に失敗しました")
-    //     }
-    //     console.log(e)
-    //   })
-  }
+    console.log(values);
+    // ここにAPI呼び出しを追加します
+  };
 
   return (
     <>
@@ -125,61 +123,80 @@ const DeckEditPage = () => {
       <div className="flex flex-col items-center">
         <div className="flex w-main flex-col items-center">
           <div className="my-10 w-96 rounded-md bg-white p-4">
-            <button
-              className=""
-              onClick={handleFavoriteChange}>
-              {deck.isFavorite ?
-                (<img className="size-8" src="/isFavorite_true.svg" alt="" />) :
-                (<img className="size-8" src="/isFavorite_false.svg" alt="" />)
-              }</button>
+            <button className="" onClick={handleFavoriteChange}>
+              {deck.isFavorite ? (
+                <img className="size-8" src="/isFavorite_true.svg" alt="" />
+              ) : (
+                <img className="size-8" src="/isFavorite_false.svg" alt="" />
+              )}
+            </button>
             <div className="flex flex-col items-center ">
               <div className="mb-4 text-4xl">{deck.name}</div>
               <div className="flex items-center">
                 <div>
-                  <input type="radio" name="publicness" id="isPublic" value="true" checked={deck.isPublic} onChange={handlePublicChange} />
-                  <label htmlFor="isPublic" className="ml-2 mr-8">公開</label>
+                  <input
+                    type="radio"
+                    name="publicness"
+                    id="isPublic"
+                    value="true"
+                    checked={deck.isPublic}
+                    onChange={handlePublicChange}
+                  />
+                  <label htmlFor="isPublic" className="ml-2 mr-8">
+                    公開
+                  </label>
                 </div>
                 <div>
-                  <input type="radio" name="publicness" id="isNotPublic" value="false" checked={!deck.isPublic} onChange={handlePublicChange} />
-                  <label htmlFor="isNotPublic" className="ml-2">非公開</label>
+                  <input
+                    type="radio"
+                    name="publicness"
+                    id="isNotPublic"
+                    value="false"
+                    checked={!deck.isPublic}
+                    onChange={handlePublicChange}
+                  />
+                  <label htmlFor="isNotPublic" className="ml-2">
+                    非公開
+                  </label>
                 </div>
               </div>
             </div>
           </div>
 
           <div className="mb-40 flex w-main flex-col items-center">
-            <form className="flex flex-col items-center"
-            onSubmit={handleSubmit(onSubmit)}>
+            <form className="flex flex-col items-center" onSubmit={handleSubmit(onSubmit)}>
               <table className="">
                 <thead>
                   <tr>
-                    {locales.map(locale => (
-                      <th className="border-b border-primary-light p-4"
-                        key={locale.id}>
+                    {locales.map((locale) => (
+                      <th className="border-b border-primary-light p-4" key={locale.id}>
                         {locale.name}
                       </th>
                     ))}
                   </tr>
                 </thead>
                 <tbody>
-                  {cards.map(card => (
+                  {cards.map((card) => (
                     <tr key={card.id}>
-                      {card.details.map(detail => (
-                        <td className="border-b border-primary-light px-10 py-4"
-                          key={detail.id}>
+                      {card.details.map((detail) => (
+                        <td className="border-b border-primary-light px-10 py-4" key={detail.id}>
                           {detail.word}
                         </td>
                       ))}
                     </tr>
                   ))}
                   <tr>
-                    {locales.map(locale => (
-                      <td className="border-b border-primary-light px-10"
-                        key={locale.id}>
-                        <textarea className="p-2"
-                        {...register('word')}
-                          name="" id="word" />
-                          {errors.word && <div className="mt-2 text-sm text-error">{errors.word.message}</div>}
+                    {fields.map((field, index) => (
+                      <td className="border-b border-primary-light px-10" key={field.id}>
+                        <textarea
+                          className="p-2"
+                          {...register(`words.${index}.word` as const, { required: 'This field is required' })}
+                        />
+                        {errors.words?.[index]?.word && (
+                          <div className="mt-2 text-sm text-error">
+                            {errors.words[index].word?.message}
+                          </div>
+                        )}
                       </td>
                     ))}
                   </tr>
@@ -197,7 +214,7 @@ const DeckEditPage = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default DeckEditPage
+export default DeckEditPage;
