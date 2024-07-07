4c820187957bde0ad816c415aaa86df789b81129
Mutsu Bojiiwa <oxidation777@gmail.com>
2024-07-07 16:46:06 +0900

    単語の編集機能を実装

--- src/pages/decks/editWordModal.tsx
+++ src/pages/decks/editWordModal.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-// import { Api } from '@/api/ApiWrapper';
+import { Api } from '@/api/ApiWrapper';
+import { useForm } from "react-hook-form"
+import z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 interface CardDetail {
   id: number
@@ -12,8 +15,18 @@ interface EditWordModalProps {
   cardDetail: CardDetail;
 }
 
+interface FormValues {
+  word: string
+}
+
+const FormSchema = z.object({
+  word: z
+    .string({
+      required_error: '単語を入力してください'
+    })
+})
+
 const EditWordModal: React.FC<EditWordModalProps> = ({ isOpen, onClose, cardDetail }) => {
-  if (!isOpen) return null;
 
   const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget) {
@@ -21,18 +34,34 @@ const EditWordModal: React.FC<EditWordModalProps> = ({ isOpen, onClose, cardDeta
     }
   };
 
-  const handleUpdate = () => {
-    // const card = cards[editCardIndex];
-    // console.log(card);
-    // Api.put(`/cards/${card.id}`)
-    //   .then((res) => {
-    //     console.log("delete OK");
-    //     console.log(res);
-    //   })
-    //   .catch((e) => {
-    //     console.log(e);
-    //   });
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty },
+  } = useForm<FormValues>({
+    resolver: zodResolver(FormSchema),
+  })
+
+  if (!isOpen) return null;
+
+  const onSubmit = (values: FormValues) => {
+    const newDetail = {
+      ...cardDetail,
+      word: values.word
+    }
+    
+    console.log(newDetail)
+    Api.put(`/cards/details/update/${newDetail.id}`, newDetail)
+      .then((res) => {
+        console.log("put OK");
+        console.log(res);
+        // setDeck(newDeck)
+        onClose()
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 
   return (
     <div
@@ -47,13 +76,27 @@ const EditWordModal: React.FC<EditWordModalProps> = ({ isOpen, onClose, cardDeta
           &times;
         </button>
         <p>単語編集</p>
-        <p>{cardDetail.word}</p>
-        <button
-          className="mt-10 w-40 rounded bg-primary px-4 py-2 text-white"
-          onClick={handleUpdate}
-        >
-          保存
-        </button>
+        <form className='mb-4 flex flex-col items-center'
+          onSubmit={handleSubmit(onSubmit)}>
+          <input
+            {...register('word', { required: 'Word is required' })}
+            type="text"
+            id="word"
+            className='w-full p-2'
+            placeholder="単語を入力..."
+            defaultValue={cardDetail.word}
+            required
+          />
+            {errors.word && <div className="mt-2 text-sm text-error">{errors.word.message}</div>}
+
+          <button
+            className="w-40 rounded bg-primary px-4 py-2 text-white"
+            type="submit"
+            disabled={!isDirty}
+          >
+            保存
+          </button>
+        </form>
         <button
           className="mt-10 w-40 rounded bg-gray-400 px-4 py-2 text-white"
           onClick={onClose}
